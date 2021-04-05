c5bae0b3ae1f0fcde51d26791e5d86773b251bb9
Vladyslav <vladislav.oksonenko@gmial.com>
2021-04-05 23:43:07 +0300

    add buttons func

--- 14-generators/main.js
+++ 14-generators/main.js
@@ -12,23 +12,34 @@ const idGenerator = createIdGenerator();
 //Завдання 2 (advanced)
 //Створіть генератор, який буде регулювати розміри шрифту для вашого сайту. (Можна допрацювати, щоб реально змінював шрифт, але це не є обов'язковою умовою)
 
-let fontSize = 14;
 
-function* newFontGenerator(pointer) {
-  while (true) {
-    if (pointer === "up") {
+const buttons = document.querySelectorAll("button");
+const body = document.querySelector("body");
+
+buttons[0].addEventListener("click", function () {
+  console.log("up", fontGenerator.next("up").value);
+  body.style.fontSize = `${fontGenerator.next("up").value}px`;
+})
+
+buttons[1].addEventListener("click", function () {
+  console.log("down", fontGenerator.next("down").value);
+  body.style.fontSize = `${fontGenerator.next("down").value}px`;
+})
+
+function* newFontGenerator(fontSize = 14) {
+  while (fontSize > 0 && fontSize < 100) {
+    const res = yield fontSize;
+    if (res === "up") {
       fontSize += 2;
       yield fontSize;
-    } else if (pointer === "down") {
+    } else if (res === "down") {
       fontSize -= 2;
       yield fontSize;
-    } else {
-      yield fontSize;
     }
-  } 
+
+  }
 }
 
 const fontGenerator = newFontGenerator();
-const fontGeneratorDown = newFontGenerator("down");
 
 
--- 15-webpack/webpack.config.js
+++ 15-webpack/webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
+    
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
